a4d447fda1d2af2cdf84086467b011070753d407
lschrafs <lschrafs@student.42wolfsburg.de>
2022-10-11 15:00:26 +0200

    ex00 progress

---
+++ ex00/src/Converter.hpp
@@ -0,0 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Converter.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lschrafs <lschrafs@student.42wolfsburg.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/11 09:06:49 by lschrafs          #+#    #+#             */
+/*   Updated: 2022/10/11 14:13:59 by lschrafs         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CONVERTER_H
+#define CONVERTER
+
+#include <iostream>
+#include <string>
+#include <climits>
+
+enum e_type {
+	UNDETERMINED,
+	INVALID,
+	CHAR,
+	INT,
+	FLOAT,
+	DOUBLE,
+	NAN,
+	INF
+};
+
+class Converter {
+ public:
+  // Constructors
+  Converter();
+  Converter(const Converter &copy);
+
+  // Destructor
+  ~Converter();
+
+  // Operators
+  Converter &operator=(const Converter &other);
+
+  // Getters & Setters
+  std::string get_arg(void) const;
+  void set_arg(const std::string &str);
+	e_type get_type(void) const;
+
+  // Functions
+  void print_conversions(void);
+
+ private:
+  std::string arg_;
+	e_type type_;
+	void determine_type_(void);
+	void determine_int_double_float_(std::string str);
+	void print_conversions_char_(void) const;
+	void print_conversions_int_(void) const;
+	void print_conversions_double_(void) const;
+	void print_conversions_float_(void) const;
+	void print_conversions_nan_(void) const;
+	void print_conversions_inf_(void) const;
+};
+
+#endif
--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -6,17 +6,18 @@
 /*   By: lschrafs <lschrafs@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/07 15:02:44 by lschrafs          #+#    #+#             */
-/*   Updated: 2022/10/07 15:09:03 by lschrafs         ###   ########.fr       */
+/*   Updated: 2022/10/11 13:49:53 by lschrafs         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
-#include <string>
+#include "Converter.hpp"
 
 int main(int argc, char **argv) {
   if (argc != 2)
     std::cout << "Usage: [./convert <char, int, float or double>]" << std::endl;
 	std::string str = argv[1];
-	if (str.length() == 1)
-	std::cout << str << std::endl;
+	
+	Converter converter = Converter();
+	converter.set_arg(str);
+	converter.print_conversions();
 }
